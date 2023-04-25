6703595e396f57403bedb6181105701be79bcfac
Hny0305Lin <1553809191@qq.com>
2023-04-25 17:36:03 +0800

    💖Android UI 添加

--- LIVING_SOIL_Android/app/src/main/java/com/livingsoil/huoturang/MainActivity.java
+++ LIVING_SOIL_Android/app/src/main/java/com/livingsoil/huoturang/MainActivity.java
@@ -1,9 +1,14 @@
 package com.livingsoil.huoturang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.livingsoil.huoturang.R.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toast.makeText(this, "活土壤 OK",Toast.LENGTH_SHORT).show();
     }
+
 }
